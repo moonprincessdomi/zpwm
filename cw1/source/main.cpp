ea1bdfad3cc4da8eee8a0e93641cc5935321699b
moonprincessdomi <moonprincessdomi@gmail.com>
2017-03-16 21:49:54 +0100

    poprawka 1 zmiana przedzialow i dodanie okna poczatkowego

--- cw1/source/main.cpp
+++ cw1/source/main.cpp
@@ -1,9 +1,8 @@
 #include<string>
-//#include <iostream>
 #include <Windows.h>
 
 void bigger_smaller(int a, int b) {
-	int c = (a + b + 1) / 2;
+	int c = (a + b) / 2;
 
 	char txt[100];
 
@@ -12,22 +11,23 @@ void bigger_smaller(int a, int b) {
 		MessageBox(0, txt, "Wygralem!", MB_OK);
 		return;
 	}
-	//std::string txt = "Czy twoja liczba jest wieksza badz rowna " + c;
-	sprintf_s(txt, "Czy twoja liczba jest wieksza badz rowna %d", c);
+
+	sprintf_s(txt, "Czy twoja liczba jest wieksza %d", c);
 	int odp = MessageBox(0, txt,"I tak wygram", MB_YESNO);
 
 	if (odp == IDYES) {
-		bigger_smaller(c, b);
+		bigger_smaller(c + 1, b);
 	}
 	else {
-		bigger_smaller(a, c - 1);
+		bigger_smaller(a, c);
 	}
 	return;
 }
 
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
-	int min = 0;
+	int min = 1;
 	int max = 40;
+	MessageBox(0, "Wybierz liczbê od 1-40", "Witaj!", MB_OK);
 	bigger_smaller(min, max);
 	return 0;
 }
